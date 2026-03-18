01a05dd0d3d1970725e8e4c52dfde649b9fa4ccb
laragarg <laragarg13@gmail.com>
2026-03-18 21:48:38 +0530

    fix: reposition profile image to show full body shot

--- components/hero.tsx
+++ components/hero.tsx
@@ -37,7 +37,7 @@ export function Hero() {
                 alt="Lara Garg"
                 width={224}
                 height={224}
-                className="w-full h-full object-cover"
+                className="w-full h-full object-cover object-[70%_20%]"
                 priority
               />
             </div>
