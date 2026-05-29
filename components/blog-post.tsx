373d7438f7a41bb6babe9dd173db852af6b7cae5
Lara Garg <laragarg13@gmail.com>
2026-05-29 13:54:55 +0530

    sports footwear blog10 and changes in syntax rendering

--- components/blog-post.tsx
+++ components/blog-post.tsx
@@ -52,8 +52,11 @@ export function BlogPost({ post }: BlogPostProps) {
             h1: (props) => <h1 className="text-3xl font-bold mt-8 mb-4 text-white" {...props} />,
             h2: (props) => <h2 className="text-2xl font-semibold mt-6 mb-3 text-white" {...props} />,
             h3: (props) => <h3 className="text-xl font-medium mt-4 mb-2 text-white" {...props} />,
+            h4: (props) => <h4 className="text-lg font-medium mt-4 mb-2 text-white" {...props} />,
             p: (props) => <p className="mb-4 text-gray-300 leading-relaxed" {...props} />,
-            li: (props) => <li className="ml-4" {...props} />,
+            ul: (props) => <ul className="list-disc pl-5 mb-4 text-gray-300 space-y-2" {...props} />,
+            ol: (props) => <ol className="list-decimal pl-5 mb-4 text-gray-300 space-y-2" {...props} />,
+            li: (props) => <li className="leading-relaxed" {...props} />,
             em: (props) => <em className="italic" {...props} />,
           }}
         >
